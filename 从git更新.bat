85071a44253736217aeb9b04eee3b0abe06f8ac6
白萝的猫 <1174565384@qq.com>
2025-03-23 10:24:57 +0800

    更新

--- 从git更新.bat
+++ 从git更新.bat
@@ -3,9 +3,5 @@ chcp 65001 >nul
 git reset --hard
 git clean -fd
 git pull https://github.com/qq1174565384/wan2.1_WebUI.git
-if %errorlevel% neq 0 (
-    echo 更新失败，请检查网络或仓库地址。
-) else (
-    echo 更新完成。
-)
+
 pause    

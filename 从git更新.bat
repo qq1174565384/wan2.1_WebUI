206c61205aed9954905b12f3abdeba3a7193734d
白萝的猫 <1174565384@qq.com>
2025-03-25 21:02:06 +0800

    更新

--- 从git更新.bat
+++ 从git更新.bat
@@ -1,7 +1,7 @@
 REM 设置UTF-8编码。
 chcp 65001 >nul
-git reset --hard
-git clean -fd
+git fetch --all  
+git reset --hard origin/main
 git pull https://github.com/qq1174565384/wan2.1_WebUI.git
 
 pause    
